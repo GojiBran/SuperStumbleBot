9d5985c64ad656df612d754d1b6484bc36271772
Goji <12769540+GojiBran@users.noreply.github.com>
2025-02-15 15:18:54 -0800

    Update SuperStumbleBot.js

--- SuperStumbleBot.js
+++ SuperStumbleBot.js
@@ -1,17 +1,6 @@
 // ==UserScript==
-// @name         StumbleBot
-// @namespace    StumbleBot
-// @version      1.0
-// @description  Play youtube videos from the chat box and/or add custom commands to StumbleChat
-// @author       Goji
-// @match        https://stumblechat.com/room/*
-// ==/UserScript==
-// The StumbleChat Bot is a UserScript written in JavaScript designed to enhance the user experience on StumbleChat, a web-based chat platform, by adding additional functionality for handling specific chat messages and enabling media playback within the chat room.
-// The script modifies the behavior of WebSocket communication to intercept and manipulate messages. It establishes a WebSocket connection with the chat server and overrides the send method to intercept outgoing messages.
-
-// ==UserScript==
-// @name         StumbleBot
-// @namespace    StumbleBot
+// @name         SuperStumbleBot
+// @namespace    SuperStumbleBot
 // @version      1.0
 // @description  Play youtube videos from the chat box and/or add custom commands to StumbleChat
 // @author       Goji
